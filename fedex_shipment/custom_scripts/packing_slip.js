cc2d6f61bdcace03779a99eb1ee4263d2ec12d01
olhonko <olhonko@gmail.com>
2015-08-12 17:09:35 +0300

    Added old custom refresh and reload window

--- fedex_shipment/custom_scripts/packing_slip.js
+++ fedex_shipment/custom_scripts/packing_slip.js
@@ -1,7 +1,12 @@
+var old_custom_refresh = cur_frm.cscript.custom_refresh;
+
 cur_frm.cscript.custom_refresh = function(doc, dt, dn) {
     if(doc.docstatus==0 && !doc.__islocal && !doc.fedex_shipment) {
         cur_frm.add_custom_button(__('Make Fedex Shipment'),
             cur_frm.cscript['Make Fedex Shipment'], frappe.boot.doctype_icons["Fedex Shipment"]);
+    	if (old_custom_refresh) {
+    		old_custom_refresh(doc, dt, dn);
+	    }
     }
 }
 
@@ -10,4 +15,8 @@ cur_frm.cscript['Make Fedex Shipment'] = function() {
         method: "fedex_shipment.shipment.make_fedex_shipment",
         frm: cur_frm
     })
+}
+
+cur_frm.cscript.on_submit = function(doc, сdt, сdn) {
+    window.location.reload();
 }
