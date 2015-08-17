bc8d2bb375fa77de2da2bb3dcd13a262df2b0c3a
olhonko <olhonko@gmail.com>
2015-08-17 15:12:46 +0300

    Fixed old custom refresh

--- fedex_shipment/custom_scripts/packing_slip.js
+++ fedex_shipment/custom_scripts/packing_slip.js
@@ -4,9 +4,9 @@ cur_frm.cscript.custom_refresh = function(doc, dt, dn) {
     if(doc.docstatus==0 && !doc.__islocal && !doc.fedex_shipment) {
         cur_frm.add_custom_button(__('Make Fedex Shipment'),
             cur_frm.cscript['Make Fedex Shipment'], frappe.boot.doctype_icons["Fedex Shipment"]);
-    	if (old_custom_refresh) {
-    		old_custom_refresh(doc, dt, dn);
-	    }
+    }
+    if (old_custom_refresh) {
+        old_custom_refresh(doc, dt, dn);
     }
 }
 
