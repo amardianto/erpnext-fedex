19a80ab2c960113cb6c07e1c0afa64f6817c2552
olhonko <olhonko@gmail.com>
2015-08-26 11:57:44 +0300

    Fixed error with message

--- fedex_shipment/shipment.py
+++ fedex_shipment/shipment.py
@@ -630,7 +630,8 @@ def delete(doc_fedex_shipment):
     if del_request.response.HighestSeverity == "SUCCESS":
         frappe.msgprint('Shipment with tracking number %s is deleted successfully.' % doc_fedex_shipment.tracking_number)
     else:
-        frappe.throw(del_request.response.Message)
+        for notification in del_request.response.Notifications:
+            frappe.throw(notification.Message)
 
 
 def track(fedex_settings, track_number):
