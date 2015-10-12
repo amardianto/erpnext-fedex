963f8d9d365f7560c5eea91b0a361c3a2f5ca3d7
olhonko <olhonko@gmail.com>
2015-10-13 00:02:53 +0300

    Added picking items to packing slip.

--- fedex_shipment/custom_scripts/packing_slip.js
+++ fedex_shipment/custom_scripts/packing_slip.js
@@ -2,8 +2,10 @@ var old_custom_refresh = cur_frm.cscript.custom_refresh;
 
 cur_frm.cscript.custom_refresh = function(doc, dt, dn) {
     if(doc.docstatus==0 && !doc.__islocal && !doc.fedex_shipment) {
-        cur_frm.add_custom_button(__('Make Fedex Shipment'),
-            cur_frm.cscript['Make Fedex Shipment'], frappe.boot.doctype_icons["Fedex Shipment"]);
+        if (doc.is_picking_stopped && doc.is_picking_started) {
+            cur_frm.add_custom_button(__('Make Fedex Shipment'),
+                cur_frm.cscript['Make Fedex Shipment'], frappe.boot.doctype_icons["Fedex Shipment"]);
+        }
     }
     if (old_custom_refresh) {
         old_custom_refresh(doc, dt, dn);
